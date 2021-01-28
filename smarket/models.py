16f83011ca69fb1033453e970e75d875adcd15ab
Aldrick <aldrick.kronos@gmail.com>
2021-01-28 11:37:06 -0300

    Alteração no model Usuario

--- smarket/models.py
+++ smarket/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Usuario(models.Model):
-    nome = models.CharField(max_length=50, unique=True)
+    nome = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nome
